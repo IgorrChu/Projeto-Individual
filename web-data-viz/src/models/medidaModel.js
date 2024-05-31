608d37ed7616652e5ed22295d332da25dec10853
igorrchu <igor.chu@sptech.school>
2024-05-31 12:02:38 -0300

    ajustes

--- web-data-viz/src/models/medidaModel.js
+++ web-data-viz/src/models/medidaModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config");
 
 function buscarUltimasMedidas(limite_linhas) {
 
-    var instrucaoSql = `select nomeUsuario, acertos, erros from usuario join quiz on idUsuario = fkUsuario ORDER BY idUsuario DESC LIMIT ${limite_linhas}`;
+    var instrucaoSql = `select nomeUsuario, acertos, erros from usuario join quiz on idUsuario = fkUsuario ORDER BY idQuiz DESC LIMIT ${limite_linhas}`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
